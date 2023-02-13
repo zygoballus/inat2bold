80ab56e4544cb7b0499b8d5795cea23af855c2e0
kaldari <119077481+zygoballus@users.noreply.github.com>
2023-02-13 13:38:40 -0500

    fleshing out collection data

--- inat2bold.php
+++ inat2bold.php
@@ -5,9 +5,10 @@ ini_set( 'display_errors', 1 );
 ini_set( 'max_execution_time', 600 );
 
 $useragent = 'iNat2BOLD Script/1.0';
-$inatapi = 'https://api.inaturalist.org/v1/observations/';
+$inatapi = 'https://api.inaturalist.org/v1/';
 $errors = [];
 $observationdata = [];
+$guess = true;
 
 /**
  * Make curl request using the passed URL
@@ -30,16 +31,60 @@ function make_curl_request( $url = null ) {
     }
 }
 
-function get_observation_data( $observationid ) {
+function get_country( $placeids, $observationid ) {
+	global $inatapi, $errors;
+	$placelist = implode( ',', $placeids );
+	$url = $inatapi . 'places/' . $placelist . '?admin_level=0'; // admin_level 0 is country
+	$inatdata = make_curl_request( $url );
+	if ( $inatdata && $inatdata['results'] && $inatdata['results'][0] && $inatdata['results'][0]['name'] ) {
+		return $inatdata['results'][0]['name'];
+	} else {
+		$errors[] = 'Country not found for observation ' . $observationid . '.';
+		return null;
+	}
+}
+
+function get_state( $placeids, $observationid ) {
+	global $inatapi, $errors;
+	$placelist = implode( ',', $placeids );
+	$url = $inatapi . 'places/' . $placelist . '?admin_level=10'; // admin_level 10 is state/province/district
+	$inatdata = make_curl_request( $url );
+	if ( $inatdata && $inatdata['results'] && $inatdata['results'][0] && $inatdata['results'][0]['name'] ) {
+		return $inatdata['results'][0]['name'];
+	} else {
+		$errors[] = 'State not found for observation ' . $observationid . '.';
+		return null;
+	}
+}
+
+function get_region( $placeids, $observationid ) {
+	global $inatapi, $errors;
+	$placelist = implode( ',', $placeids );
+	$url = $inatapi . 'places/' . $placelist . '?admin_level=20'; // admin_level 20 is county/region
+	$inatdata = make_curl_request( $url );
+	if ( $inatdata && $inatdata['results'] && $inatdata['results'][0] && $inatdata['results'][0]['name'] ) {
+		return $inatdata['results'][0]['name'];
+	} else {
+		return null;
+	}
+}
+
+function get_observation_data( $observationid, $guessplace ) {
 	global $inatapi, $errors;
 	$data = [];
 	if ( $observationid ) {
-		$url = $inatapi . $observationid;
+		$url = $inatapi . 'observations/' . $observationid;
 		$inatdata = make_curl_request( $url );
 		if ( $inatdata && $inatdata['results'] && $inatdata['results'][0] ) {
 			$results = $inatdata['results'][0];
 			$montharray = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
 			$data['collection_date'] = $results['observed_on_details']['day'] . '-' . $montharray[$results['observed_on_details']['month']] . '-' . $results['observed_on_details']['year'];
+			$data['country'] = get_country( $results['place_ids'], $observationid );
+			$data['state'] = get_state( $results['place_ids'], $observationid );
+			$data['region'] = get_region( $results['place_ids'], $observationid );
+			if ( $guessplace ) {
+				$data['exact_site'] = $results['place_guess'];
+			}
 			$location = explode( ',', $results['location'] );
 			$data['latitude'] = $location[0];
 			$data['longitude'] = $location[1];
@@ -57,6 +102,11 @@ function get_observation_data( $observationid ) {
 if ( $_POST ) {
 	// If an observation was posted, look up the data.
 	if ( isset( $_POST['observations'] ) ) {
+		if ( isset( $_POST['guess'] ) ) {
+			$guessplace = true;
+		} else {
+			$guessplace = false;
+		}
 		$observationlist = explode( "\n", $_POST['observations'] );
 		// Limit to 96 observations.
 		$observationlist = array_slice( $observationlist, 0, 96 );
@@ -64,8 +114,13 @@ if ( $_POST ) {
 		foreach ( $observationlist as $observationid ) {
 			if ( preg_match( '/\d+/', $observationid, $matches ) ) {
 				$observationid = $matches[0];
-				$observationdata[$a] = get_observation_data( $observationid );
+				$observationdata[$a] = get_observation_data( $observationid, $guessplace );
 				if ( isset( $_POST['collectors'] ) ) $observationdata[$a]['collectors'] = $_POST['collectors'];
+				if ( isset( $_POST['gps_source'] ) ) $observationdata[$a]['gps_source'] = $_POST['gps_source'];
+				if ( isset( $_POST['habitat'] ) ) $observationdata[$a]['habitat'] = $_POST['habitat'];
+				if ( isset( $_POST['sampling_protocol'] ) ) $observationdata[$a]['sampling_protocol'] = $_POST['sampling_protocol'];
+				if ( isset( $_POST['site_code'] ) ) $observationdata[$a]['site_code'] = $_POST['site_code'];
+				if ( isset( $_POST['collection_event_id'] ) ) $observationdata[$a]['collection_event_id'] = $_POST['collection_event_id'];
 			} else {
 				$observationdata[$a] = null;
 			}
@@ -116,6 +171,9 @@ body {
 td.nowrap {
 	white-space: nowrap;
 }
+p.optionaldata {
+	line-height: 1.5;
+}
 </style>
 <script src="./jquery.min.js"></script>
 <script type="text/javascript">
@@ -133,9 +191,22 @@ $(document).ready(function () {
 <p>
 	Observation List (1 per line, max 96):<br/><textarea rows="5" cols="50" name="observations"></textarea>
 </p>
-<p>
-	<label for="collectors">Collectors (optional):</label>
-	<input type="text" id="collectors" name="collectors" />
+<p class="optionaldata">
+	<input type="checkbox" id="guess" name="guess" <?php if ($guess) echo "checked";?> value="yes">
+	<label for="guess">Map iNaturalist place guess to BOLD exact site.</label><br/>
+	Optional data (not supplied by iNaturalist):<br/>
+	&nbsp;&nbsp;&nbsp;&nbsp;<label for="collectors">Collectors:</label>
+	<input type="text" id="collectors" name="collectors" /><br/>
+	&nbsp;&nbsp;&nbsp;&nbsp;<label for="gps_source">GPS source:</label>
+	<input type="text" id="gps_source" name="gps_source" /><br/>
+	&nbsp;&nbsp;&nbsp;&nbsp;<label for="habitat">Habitat:</label>
+	<input type="text" id="habitat" name="habitat" /><br/>
+	&nbsp;&nbsp;&nbsp;&nbsp;<label for="sampling_protocol">Sampling protocol:</label>
+	<input type="text" id="sampling_protocol" name="sampling_protocol" /><br/>
+	&nbsp;&nbsp;&nbsp;&nbsp;<label for="site_code">Site code:</label>
+	<input type="text" id="site_code" name="site_code" /><br/>
+	&nbsp;&nbsp;&nbsp;&nbsp;<label for="collection_event_id">Collection event ID:</label>
+	<input type="text" id="collection_event_id" name="collection_event_id" /><br/>
 </p>
 <input class="submitbtn" type="submit" />
 </form>
@@ -153,6 +224,7 @@ if ( $errors ) {
 if ( $observationdata ) {
 
 // Collection Data Table
+	print( '<h2>Collection Data</h2>' );
 	print( '<table class="resulttable" border="0" cellpadding="5" cellspacing="10">' );
 	print( '<tr><th>Collectors</th><th>Collection Date</th><th>Country/Ocean</th><th>State/Province</th><th>Region</th><th>Sector</th><th>Exact Site</th><th>Latitude</th><th>Longitude</th><th>Elevation</th><th>Depth</th><th>Elevation Precision</th><th>Depth Precision</th><th>GPS Source</th><th>Coordinate Accuracy</th><th>Event Time</th><th>Collection Date Accuracy</th><th>Habitat</th><th>Sampling Protocol</th><th>Collection Notes</th><th>Site Code</th><th>Collection Event ID</th></tr>' );
 
